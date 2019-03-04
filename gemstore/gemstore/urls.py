7b27ab32345544bce67a83087051d79b06b0c0d2
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-04 11:41:39 -0600

    Include accounts.urls to gemstore/url.py

--- gemstore/gemstore/urls.py
+++ gemstore/gemstore/urls.py
@@ -20,6 +20,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path('accounts/',include('accounts.urls', namespace='accounts')),
+    path('accounts/',include('django.contrib.auth.urls')),
+
 
 ]
